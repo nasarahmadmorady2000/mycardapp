251f12bd86e2d6a16801a05ded3eabb3de0d2995
Nesarahmad <nasarahmemo12345@gmail.com>
2026-07-25 13:29:37 +0430

    adding icaon to the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,35 +14,93 @@ class Mycard extends StatelessWidget {
           backgroundColor: Colors.teal[900],
           title: Center(child: Text("My Card App")),
         ),
-        body: Center(child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                color: Colors.red,
-                height:100,
-                width:150,
-                child:Text("container 1",style:TextStyle(fontSize:20,color:Colors.yellow),),
-              ),
-              Container(
-                color: Colors.blue,
-                height:100,
-                width:150,
-                child:Text("container 2",style:TextStyle(fontSize:20,color:Colors.yellow),),
-              ),
-              Container(
-                height:100,
-                width:150,
-
-                color:Colors.yellow,
-                child:Text("container 3",style:TextStyle(fontSize: 20,color:Color.fromARGB(255, 228, 6, 154)))
-              )
-            ],
-          ),
-          
-        ),),
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  child: ClipOval(
+                    child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Image.asset(
+                        "assets/images/name1.JPG",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "NesarAhmad",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: "Pacifico",
+                  ),
+                ),
+                SizedBox(height:10),
+                Text(
+                  "flutter Developer",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontFamily: "SourceSans3",
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+               Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child:Row(
+                  children:[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal[900],
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "+93 781714886",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                        fontFamily: "SourceSans3",
+                      ),
+                    )
+                  ]
+                )
+               ),
+                Container(
+                  color: Colors.white,
+                padding:EdgeInsets.all( 10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child:Row(
+                  children:[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal[900],
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "nesar.ahmad@example.com",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal[900],
+                        fontFamily: "SourceSans3",
+                      ),
+                    )
+                  ]
+                ),
+               ),
+                
+        ],
       ),
-    );
+     ) ) ));
   }
 }
